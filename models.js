35ec1c86fe4f66abfa4164f58348b2c453b2a463
vcordero07 <vcordero07@gmail.com>
2017-09-05 13:54:11 -0400

    wip

--- models.js
+++ models.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 
 // this is our schema to represent a restaurant
 const restaurantSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  borough: {type: String, required: true},
-  cuisine: {type: String, required: true},
+  name: {
+    type: String,
+    required: true
+  },
+  borough: {
+    type: String,
+    required: true
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
   address: {
     building: String,
     // coord will be an array of string values
@@ -19,18 +28,21 @@ const restaurantSchema = mongoose.Schema({
     score: Number
   }]
 });
-
+//this is a test
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
 // allow us to define properties on our object that manipulate
 // properties that are stored in the database. Here we use it
 // to generate a human readable string based on the address object
 // we're storing in Mongo.
 restaurantSchema.virtual('addressString').get(function() {
-  return `${this.address.building} ${this.address.street}`.trim()});
+  return `${this.address.building} ${this.address.street}`.trim()
+});
 
 // this virtual grabs the most recent grade for a restaurant.
 restaurantSchema.virtual('grade').get(function() {
-  const gradeObj = this.grades.sort((a, b) => {return b.date - a.date})[0] || {};
+  const gradeObj = this.grades.sort((a, b) => {
+    return b.date - a.date
+  })[0] || {};
   return gradeObj.grade;
 });
 
@@ -53,4 +65,6 @@ restaurantSchema.methods.apiRepr = function() {
 // schema must be defined *before* we make the call to `.model`.
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
-module.exports = {Restaurant};
+module.exports = {
+  Restaurant
+};
